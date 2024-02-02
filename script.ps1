71cdefa9f8fa7c5165c553998349a623fba001d5
shahhussain88 <80907093+shahhussain88@users.noreply.github.com>
2024-02-02 18:13:23 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,3 @@
 
 az aks get-credentials --resource-group 'tsudev-rg' --name 'tsudev-aks'
-helm install alb-controller oci://mcr.microsoft.com/application-lb/charts/alb-controller --namespace default --version 0.6.3 --set albController.namespace=azure-alb-system  --set albController.podIdentity.clientID=$(az identity show -g $RESOURCE_GROUP -n azure-alb-identity --query clientId -o tsv)
+helm install alb-controller oci://mcr.microsoft.com/application-lb/charts/alb-controller --namespace default --version 0.6.3 --set albController.namespace=azure-alb-system  --set albController.podIdentity.clientID=$(az identity show -g 'tsudev-rg' -n azure-alb-identity --query clientId -o tsv)
