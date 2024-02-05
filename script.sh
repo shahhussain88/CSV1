c8253863c9666190dfb90b8906b35d3ca0d12ba1
shahhussain88 <80907093+shahhussain88@users.noreply.github.com>
2024-02-06 00:00:12 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,1 @@
-
-az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_NAME
-helm install alb-controller oci://mcr.microsoft.com/application-lb/charts/alb-controller --namespace default --version 0.6.3 --set albController.namespace=azure-alb-system  --set albController.podIdentity.clientID=$(az identity show -g $RESOURCE_GROUP -n azure-alb-identity --query clientId -o tsv)
+echo "hi" && az account show
