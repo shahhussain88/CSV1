64b89cae50f0a7ff8c69611f107a3b890bd04bd8
shahhussain88 <80907093+shahhussain88@users.noreply.github.com>
2024-02-05 16:53:47 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -14,26 +14,12 @@ az aks get-credentials \
   subscription $subscriptionId \
   only-show-errors
 
-
-#az aks get-credentials --resource-group 'tsudev-rg' --name 'tsudev-aks'
-
-curl 'https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3' -o get_helm.sh -s
-chmod 700 get_helm.sh
-./get_helm.sh &>/dev/null
-
-# Add Helm repos
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add jetstack https://charts.jetstack.io
-
-# Update Helm repos
-helm repo update
-
 applicationGatewayForContainersName='tsudev-agfc-alb'
 diagnosticSettingName="DefaultDiagnosticSettings"
 
 # Install the Application Load Balancer Controller
-command="helm upgrade alb-controller oci://mcr.microsoft.com/application-lb/charts/alb-controller \
---install \
+command="helm install alb-controller oci://mcr.microsoft.com/application-lb/charts/alb-controller \
+--namespace default \
 --version 0.6.3 \
 --set albController.namespace='azure-alb-system' \
 --set albController.podIdentity.clientID='a36394c8-dc81-4333-b145-99ab8003203d'"
@@ -46,3 +32,10 @@ command "$command"
 
 
 #helm install alb-controller oci://mcr.microsoft.com/application-lb/charts/alb-controller --namespace default --version 0.6.3 --set albController.namespace=azure-alb-system  --set albController.podIdentity.clientID=$(az identity show -g 'tsudev-rg' -n azure-alb-identity --query clientId -o tsv)
+
+# Add Helm repos
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add jetstack https://charts.jetstack.io
+
+# Update Helm repos
+helm repo update
