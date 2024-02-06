5031a45e4de4cf1e6c2cf8fdc9f42c01dbe0a8ac
shahhussain88 <80907093+shahhussain88@users.noreply.github.com>
2024-02-06 19:44:06 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,1 @@
-param([string] $name)
-      $output = 'Hello {0}. The username is {1}, the password is {2}.' -f $name,${Env:UserName},${Env:Password}
-      Write-Output $output
-      $DeploymentScriptOutputs = @{}
-      $DeploymentScriptOutputs['text'] = $output
+az login
