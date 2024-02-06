d57867a29e1c54ae66364906d803c1d075ad107b
shahhussain88 <80907093+shahhussain88@users.noreply.github.com>
2024-02-06 19:27:39 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
-# Install kubectl
-az aks install-cli --only-show-errors
+param([string] $name)
+      $output = 'Hello {0}. The username is {1}, the password is {2}.' -f $name,${Env:UserName},${Env:Password}
+      Write-Output $output
+      $DeploymentScriptOutputs = @{}
+      $DeploymentScriptOutputs['text'] = $output
